22b1bc502d985a9d00104bb3eb310c0e25e68209
ganshulong <153274152@qq.com>
2018-07-30 18:11:52 +0800

    ui 背景条颜色渐变

--- pages/company-introduction/company-introduction.js
+++ pages/company-introduction/company-introduction.js
@@ -1,14 +1,53 @@
-var app = getApp();
-const api = require('../../utils/api')
 Page({
     data:{
+
     },
-    OnClickPhoneCounsel:function(event){
+    
+    bIsTopColor: true,
+
+    onLoad: function (options) {
+        this.changeColor()
+    },
+    onClickPhoneCounsel:function(event){
         wx.makePhoneCall({
             phoneNumber: '15170057995',
             success: function () {
                 console.log("成功拨打电话")
             }
         })
+    },
+    onPageScroll:function(e){
+        if (this.bIsTopColor) {
+            if (204 < e.scrollTop) {
+                this.bIsTopColor = false;
+                this.changeColor();
+            }
+        } else {
+            if (204 >= e.scrollTop) {
+                this.bIsTopColor = true;
+                this.changeColor();
+            }
+        }
+    },
+    changeColor:function(){
+        if (this.bIsTopColor) {
+            wx.setNavigationBarColor({
+                frontColor: '#ffffff',
+                backgroundColor: '#f64500',
+                animation: {
+                    duration: 400,
+                    timingFunc: 'easeIn'
+                }
+            })
+        } else {
+            wx.setNavigationBarColor({
+                frontColor: '#ffffff',
+                backgroundColor: '#cccccc',
+                animation: {
+                    duration: 400,
+                    timingFunc: 'easeIn'
+                }
+            })
+        }
     },
 })
