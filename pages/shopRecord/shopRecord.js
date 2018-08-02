27a7ee82cb04e496765ddec0352fbf9610f6d9d4
ganshulong <153274152@qq.com>
2018-08-02 17:57:57 +0800

    + popup

--- pages/shopRecord/shopRecord.js
+++ pages/shopRecord/shopRecord.js
@@ -2,21 +2,45 @@ Page({
     data:{
         statusName:["已预约", "店家服务已完成，请确认完成"],
         shopRecordList: [
-            {shopRecordId: 23, time: "2018-07-31 16:07:00", setMealType: "《钻石卡套餐》", projectType: "1、翡翠紫瓷套装(数量x2)", carNumber:"赣A 12345", status:0},
-            {shopRecordId: 23, time: "2018-07-31 16:07:00", setMealType: "《钻石卡套餐》", projectType: "1、翡翠紫瓷套装(数量x2)", carNumber:"赣A 66666", status:1},
-            {shopRecordId: 23, time: "2018-07-31 16:07:00", setMealType: "《钻石卡套餐》", projectType: "1、翡翠紫瓷套装(数量x2)", carNumber:"赣A 88888", status:0}
+            {shopRecordId: 23, time: "2018-07-31 16:07:00", setMealType: "钻石卡套餐", projectType: "1、翡翠紫瓷套装(数量x2)", carNumber:"赣A 12345", status:0},
+            {shopRecordId: 23, time: "2018-07-31 16:07:00", setMealType: "钻石卡套餐", projectType: "1、翡翠紫瓷套装(数量x2)", carNumber:"赣A 66666", status:1},
+            {shopRecordId: 23, time: "2018-07-31 16:07:00", setMealType: "钻石卡套餐", projectType: "1、翡翠紫瓷套装(数量x2)", carNumber:"赣A 12345", status:0},
+            {shopRecordId: 23, time: "2018-07-31 16:07:00", setMealType: "钻石卡套餐", projectType: "1、翡翠紫瓷套装(数量x2)", carNumber:"赣A 66666", status:1},
+            {shopRecordId: 23, time: "2018-07-31 16:07:00", setMealType: "钻石卡套餐", projectType: "1、翡翠紫瓷套装(数量x2)", carNumber:"赣A 12345", status:0},
+            {shopRecordId: 23, time: "2018-07-31 16:07:00", setMealType: "钻石卡套餐", projectType: "1、翡翠紫瓷套装(数量x2)", carNumber:"赣A 66666", status:1},
+            {shopRecordId: 23, time: "2018-07-31 16:07:00", setMealType: "钻石卡套餐", projectType: "1、翡翠紫瓷套装(数量x2)", carNumber:"赣A 12345", status:0},
+            {shopRecordId: 23, time: "2018-07-31 16:07:00", setMealType: "钻石卡套餐", projectType: "1、翡翠紫瓷套装(数量x2)", carNumber:"赣A 66666", status:1},
         ],
+        bIsHidePopup: false,
     },
     onLoad: function (options) {
         // this.setData({
         //     shopRecordList: this.shopRecordList
         // })
     },
+    closePopupTap:function(event){
+        this.setData({
+            bIsHidePopup: true
+        })
+    },
     onClickButtonConfirm:function(event){
-        console.log("onClickButtonConfirm")
+        var animation = wx.createAnimation({
+            duration: 200,
+            timingFunction: "linear"
+        });
+        this.animation = animation;
+        animation.height(0).step();
+        this.setData({
+            bIsHidePopup: false,
+            animationData: animation.export()
+        });
+        console.log("onClickButtonConfirm:"+this.data.bIsHidePopup)
     },
     onClickButtonCancel:function(event){
-        console.log("onClickButtonCancel")
+        this.setData({
+            bIsHidePopup: true
+        })
+        console.log("onClickButtonCancel:"+this.data.bIsHidePopup)
     },
     onClickButtonContact:function(event){
         console.log("onClickButtonContact")
